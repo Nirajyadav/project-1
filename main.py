7766b7771cb2c668a2a284f4f4c844147cdf27f9
Nirajyadav <111130716+Nirajyadav@users.noreply.github.com>
2022-09-27 12:01:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Hello Niraj")
+st.title("Niraj yadav")
+st.write("MsC DSAI")
+st.header("PYTHON")
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+st.caption('This is a string that explains something above.')
+st.text('This is some text.')
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
