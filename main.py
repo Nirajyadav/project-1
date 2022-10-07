ebb18b63fa4a939795370bbc502be0163208dbd7
Nirajyadav <111130716+Nirajyadav@users.noreply.github.com>
2022-10-07 13:06:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,68 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
+import plotly.express as px
 
-st.title("Hello Niraj")
-st.title("Niraj yadav")
-st.write("MsC DSAI")
-st.header("PYTHON")
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
-st.caption('This is a string that explains something above.')
-st.text('This is some text.')
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code, language='python')
+df = pd.read_csv("C:/Users/MSCIT/Downloads/bank.csv")
+st.set_page_config(
+    page_title='Bank Marketing Dashboard',
+    page_icon='✅',
+    layout='wide'
+)
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+# dashboard title
 
-st.line_chart(chart_data)
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+st.title("Bank Marketing Dashboard")
 
-st.area_chart(chart_data)
-col1, col2, col3 = st.columns(3)
+# top-level filters
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
 
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+# creating a single-element container.
+placeholder = st.empty()
 
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+# dataframe filter
+
+df = df[df['job'] == job_filter]
+
+# near real-time / live feed simulation
+
+for seconds in range(200):
+    # while True:
+
+    df['age_new'] = df['age'] * np.random.choice(range(1, 5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1, 5))
+
+    # creating KPIs
+    avg_age = np.mean(df['age_new'])
+
+    count_married = int(df[(df["marital"] == 'married')]['marital'].count() + np.random.choice(range(1, 30)))
+
+    balance = np.mean(df['balance_new'])
+
+    with placeholder.container():
+        # create three columns
+        kpi1, kpi2, kpi3 = st.columns(3)
+
+        # fill in those three columns with respective metrics or KPIs
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta=round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value=int(count_married), delta=- 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value=f"$ {round(balance, 2)} ",
+                    delta=- round(balance / count_married) * 100)
+
+        # create two columns for charts
+
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y='age_new', x='marital')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame=df, x='age_new')
+            st.write(fig2)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
 
